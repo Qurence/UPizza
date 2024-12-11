90309612a0d6c71c6d1002c80325ed690ad936c0
Qurence <shyposhartem.biz@gmail.com>
2024-12-12 01:28:31 +0200

    complate_component_header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+// import Image from "next/image";
 
 export default function Home() {
   return <h1>
-    Hello
-    <Button variant={"outline"}>Корзина</Button>
+    Главная
     </h1>
 }
