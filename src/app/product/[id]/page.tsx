9f60e8346ea2748d9e855d8fea610e9bda538249
Qurence <shyposhartem.biz@gmail.com>
2024-12-23 20:43:16 +0200

    rendere_product_page_Preparation_Next_Group_fot_Modal

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,4 +1,51 @@
- 
-export default function ProductPage({ params: {id} } : {params: {id: string}}) {
-    return <p>Product {id}</p>
+import { notFound } from "next/navigation";
+import { prisma } from "../../../../prisma/prisma-client";
+import { Container, GroupVariants, ProductImage, Title } from "@/components/shared";
+
+export default async function ProductPage({ params: { id }, }: { params: { id: string }; }) {
+  const product = await prisma.product.findFirst({ where: { id: Number(id) } });
+  
+  if (!product) {
+    return notFound();
+  }
+
+  return <Container className="flex flex-col my-10">
+    <div className="flex flex-1">
+      <ProductImage imageUrl={product.imageUrl} size={40} />
+
+      <div className="w-[490px] bg-[#f7f6f5] p-7">
+        <Title text={product.name} size="md" className="font-extrabold mb-1" />
+        <p className="text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro at ducimus modi!</p>
+
+        <GroupVariants 
+        selectedValue="2"
+        items={[
+          {
+          name: 'Маленька',
+          value: '1',
+          },
+          {
+          name: 'Середня',
+          value: '2',
+          },
+          {
+          name: 'Велика',
+          value: '3',
+          disabled: true,
+          }
+        ]} />
+      </div>
+    </div>
+  </Container>
+  
 }
+
+// Используйте 'async' для функции страницы
+// export default async function ProductPage({
+//   params,
+// }: {
+//   params: { id: string };
+// }) {
+//   const { id } = await params; // Ожидайте получения параметров
+//   return <p>Product {id}</p>;
+// }
--- src/components/shared/categories.tsx
+++ src/components/shared/categories.tsx
@@ -3,38 +3,31 @@
 import { cn } from '@/lib/utils';
 import React from 'react';
 import { useCategoryStore } from '../../../store/category';
+import { Category } from '@prisma/client';
 
 interface Props {
   items: Category[];
   className?: string;
 }
 
-// const cats = [
-//   { id: 1, name: 'Піци' },
-//   { id: 2, name: 'Комбо' },
-//   { id: 3, name: 'Закуски' },
-//   { id: 4, name: 'Кокотейлі' },
-//   { id: 5, name: 'Кава' },
-//   { id: 6, name: 'Десерти' },
-//   { id: 7, name: 'Напої' },
-// ];
-
 export const Categories: React.FC<Props> = ({ items, className }) => {
   const categoryActiveId = useCategoryStore((state) => state.activeId);
   return (
-    <div className={cn('inline-flex gap-1 bg-gray-50 p-1 rounded-2xl', className)}>
-        {
-            items.map(({ name, id }, index) => (
-                <a className={cn(
-                    'flex items-center font-bold h-11 rounded-2xl px-5' , 
-                    categoryActiveId === id && 'bg-white shadow-md shadow-gray-200 text-primary'
-                )} 
-                href={`#${name}`}
-                key={index}>
-                    <button>{name}</button>
-                </a>
-            ))
-        }
+    <div
+      className={cn("inline-flex gap-1 bg-gray-50 p-1 rounded-2xl", className)} >
+      {items.map(({ name, id }, index) => (
+        <a
+          className={cn(
+            "flex items-center font-bold h-11 rounded-2xl px-5",
+            categoryActiveId === id &&
+              "bg-white shadow-md shadow-gray-200 text-primary"
+          )}
+          href={`#${name}`}
+          key={index}
+        >
+          <button>{name}</button>
+        </a>
+      ))}
     </div>
   );
 };
