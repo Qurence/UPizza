5526c8eeded6e6b6060fad5fde5b9987d53614dd
Qurence <shyposhartem.biz@gmail.com>
2025-01-21 00:23:18 +0200

    fixed header_border

--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -14,7 +14,7 @@ interface Props {
 
 export const Header: React.FC<Props> = ({ className }) => {
   return (
-    <div className={cn('border border-b',className)}>
+    <div className={cn('border-b border-muted',className)}>
         <div>
             <Container className='flex items-center justify-between py-8'>
 
