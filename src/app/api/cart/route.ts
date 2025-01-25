b6ac44c3b6e962d3dff47679ca4164ff273ecc23
Qurence <shyposhartem.biz@gmail.com>
2025-01-25 20:50:40 +0200

    complate api route - addCartItem

--- src/app/api/cart/route.ts
+++ src/app/api/cart/route.ts
@@ -1,5 +1,10 @@
+ 
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "../../../../prisma/prisma-client";
+import crypto from "crypto";
+import { findOrCreateCart } from "@/lib";
+import { CreateCartItemValues } from "../../../../services/dto/cart.dto";
+import { updateCartTotalAmount } from "@/lib/update-cart-total-amount";
 
 export async function GET(req: NextRequest) {
   try {
@@ -11,37 +16,90 @@ export async function GET(req: NextRequest) {
     }
 
     const userCart = await prisma.cart.findFirst({
-        where: { 
-            OR: [
-                // {
-                //     userId,
-                // },
-                {
-                    token,
-                },
-            ]
-        },
-        include: {
-            items: {
-              orderBy: {
-                createdAt: "desc",
-              },
+      where: {
+        OR: [
+          // {
+          //     userId,
+          // },
+          {
+            token,
+          },
+        ],
+      },
+      include: {
+        items: {
+          orderBy: {
+            createdAt: "desc",
+          },
+          include: {
+            productItem: {
               include: {
-                productItem: {
-                  include: {
-                    product: true,
-                  },
-                },
-                ingredients: true,
+                product: true,
               },
             },
+            ingredients: true,
+          },
         },
-
+      },
     });
 
     return NextResponse.json(userCart);
   } catch (error) {
-    console.log(error);
+    console.log("[CART_GET] Server error", error);
+    return NextResponse.json({ message: "Не удалось получить корзину" });
   }
 }
 
+export async function POST(req: NextRequest) {
+  try {
+    let token = req.cookies.get("cartToken")?.value;
+
+    if (!token) {
+      token = crypto.randomUUID();
+    }
+
+    const userCart = await findOrCreateCart(token);
+
+    const data = (await req.json()) as CreateCartItemValues;
+
+    const findCartItem = await prisma.cartItem.findFirst({
+      where: {
+        cartId: userCart.id,
+        productItemId: data.productItemId,
+        ingredients: { every: { id: { in: data.ingredients } } },
+      },
+    });
+    // Если товар уже есть в корзине, то увеличиваем его количество
+    if (findCartItem) {
+      await prisma.cartItem.update({
+        where: {
+          id: findCartItem.id,
+        },
+        data: {
+          quantity: findCartItem.quantity + 1,
+        },
+      });
+
+      // const updatedUserCart = await updateCartTotalAmount(token);
+    }
+
+    await prisma.cartItem.create({
+      data: {
+        cartId: userCart.id,
+        productItemId: data.productItemId,
+        quantity: 1,
+        ingredients: { connect: data.ingredients?.map((id) => ({ id })) },
+      },
+    });
+
+    const updatedUserCart = await updateCartTotalAmount(token);
+
+    const resp = NextResponse.json(updatedUserCart);
+    resp.cookies.set("cartToken", token);
+    return resp;
+    // return NextResponse.json(updatedUserCart);
+  } catch (error) {
+    console.log("[CART_POST] Server error", error);
+    return NextResponse.json({ message: "Не удалось создать корзину" });
+  }
+}
